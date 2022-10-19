e14f545cbb31f8224be3cc58a43a6df79077c2d0
ArchyScript <yungdansa@gmail.com>
2022-10-19 03:40:14 +0100

    first vuepress project

---
+++ src/.vuepress/components/demo-component.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="demo">
+    {{ msg }}
+  </p>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const msg = ref('hadjka')
+
+    return {
+      msg,
+    }
+  },
+}
+</script>
